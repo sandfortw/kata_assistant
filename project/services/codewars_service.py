82e525c9d8aa1f5570af4fe6308780ff76288eae
Weston Sandfort <sandfortw@gmail.com>
2023-09-29 12:36:24 -0600

    Add codewars service that works as intended

--- project/services/codewars_service.py
+++ project/services/codewars_service.py
@@ -1,6 +1,9 @@
 import requests
 from urllib.parse import quote
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException, WebDriverException
 
 class CodewarsService:
     def __init__(self, user='Weston Sandfort', page='0'):
@@ -17,24 +20,21 @@ class CodewarsService:
             return response.json()
         else:
             raise Exception(f"Error fetching challenge. Status code: {response.status_code}")
-    
-    # def get_challenge_data(challenge_id):
-    #     """Retrieve data from a challenge id"""
-    #     # 5202ef17a402dd033c000009
 
     def get_challenge_info(challenge_url):
-    # Make an HTTP request to the challenge URL
-        response = requests.get(challenge_url)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the HTML content using BeautifulSoup
-            soup = BeautifulSoup(response.content, 'html.parser')
-            import ipdb; ipdb.set_trace()
-            # Find all elements with class starting with 'cm-'
-            code_elements = soup.find_all(class_=lambda x: x and x.startswith('cm-'))
+        try:
+            driver = webdriver.Chrome() 
+            driver.get(challenge_url) 
+            response = driver.page_source
+        except (TimeoutException, WebDriverException) as e:
+            print(f"An error occurred: {e}")
+            response = None
+        
+        if response is not None:
+            soup = BeautifulSoup(response, 'html.parser')
+        
+            code_elements = soup.find_all('div', class_='CodeMirror-code')
             
-            # Extract the text content from these elements
             python_code = "\n".join(element.text.strip() for element in code_elements)
 
             return python_code.strip()
@@ -42,11 +42,3 @@ class CodewarsService:
         else:
             print(f"Error fetching challenge. Status code: {response.status_code}")
             return None
-        
-    # Example usage
-    challenge_url = 'https://www.codewars.com/kata/valid-braces/train/python'
-    python_code = get_challenge_info(challenge_url)
-
-    if python_code:
-        print("Python Code:")
-        print(python_code)
