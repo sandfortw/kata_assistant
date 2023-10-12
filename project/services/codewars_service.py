960ddcfe6ada7c619d2d21e7cbbd04d40475c246
Weston Sandfort <sandfortw@gmail.com>
2023-10-12 12:04:05 -0600

    Remove unused code

--- project/services/codewars_service.py
+++ project/services/codewars_service.py
@@ -1,10 +1,7 @@
 import requests
-# from urllib.parse import quote
-# from bs4 import BeautifulSoup
-from selenium.common.exceptions import TimeoutException, WebDriverException
+import json
 from seleniumwire import webdriver
 from seleniumwire.utils import decode
-import json
 
 
 
@@ -18,37 +15,20 @@ class CodewarsService:
         Uses chrome webdriver and beautiful soup to parse html and return code.
         """
         try:
-            driver = webdriver.Chrome()
-            # driver.implicitly_wait(5)
+            options = webdriver.ChromeOptions()
+            options.headless = True
+            driver = webdriver.Chrome(options = options)
             driver.get(challenge_url)
-            session = ''
             for request in driver.requests:
                 if request.url.split('/')[-1] == 'session':
                     session = request
             response = session.response
-            # body = json.loads(decode(response.body, response.headers.get('Content-Encoding', 'identity')))
-            # import ipdb; ipdb.set_trace()
-            # response = driver.page_source
-        except (TimeoutException, WebDriverException) as e:
+        except Exception as e:
             print(f"An error occurred: {e}")
             response = None
 
         if response is not None:
             body = json.loads(decode(response.body, response.headers.get('Content-Encoding', 'identity')))
-            # soup = BeautifulSoup(response, 'html.parser')
-            # import ipdb; ipdb.set_trace()
-            # code_elements = soup.find_all('div', class_='CodeMirror-code')
-            # # code_elements = soup.find_all('pre', class_='CodeMirror-line')
-            # challenge_elements = code_elements[0].find_all('pre', class_='CodeMirror-line')
-            # challenge_code = "\n".join(element.text
-            #                         for element in challenge_elements)
-            # test_elements = code_elements[1].find_all('pre', class_='CodeMirror-line')
-            # test_code = "\n".join(element.text
-            #                         for element in test_elements)
-            # code_elements = soup.find_all('pre', class_='CodeMirror-line')
-            # import ipdb; ipdb.set_trace()
-            # python_code = "\n".join(element.text.strip()
-            #                         for element in code_elements)
             url = f'https://www.codewars.com/api/v1/code-challenges/{challenge_url.split("/")[-3]}'
 
             challenge_info = requests.get(url).json()
